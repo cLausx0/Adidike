fc904b474bbace6c61b6b2c33e6ec2117ae58618
cLausx0 <david.almeida2707@gmail.com>
2023-06-29 20:43:44 -0300

    Empacotando classe App

---
+++ src/app/App.java
@@ -0,0 +1,51 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static Stage stage;
+    private static Scene produtosScene;
+    private static Scene clientesScene;
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+
+        Image icon = new Image(getClass().getResourceAsStream("/view/Adidike.jpg"));
+        primaryStage.getIcons().add(icon);
+
+        Parent fxmlProdutos = FXMLLoader.load(getClass().getResource("/view/telaProdutos.fxml"));
+        produtosScene = new Scene(fxmlProdutos, 900, 600);
+
+        Parent fxmlClientes = FXMLLoader.load(getClass().getResource("/view/telaClientes.fxml"));
+        clientesScene = new Scene(fxmlClientes, 900, 600);
+
+
+        primaryStage.setTitle("ADIDIKE");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(produtosScene);
+        primaryStage.show();
+
+    }
+
+    public static void mudarTela(String tela) {
+        switch (tela) {
+            case "produtos":
+                stage.setScene(produtosScene);
+                break;
+            case "clientes":
+                stage.setScene(clientesScene);
+                break;
+        }
+    }
+}
